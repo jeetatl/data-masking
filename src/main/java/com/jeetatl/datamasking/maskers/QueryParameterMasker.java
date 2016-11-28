95a32aaf105c7bb26a3b44ffabac95b7e2a78ad3
Ali K <jeetatl@gmail.com>
2016-11-28 13:07:59 -0500

    Add query masker

--- src/main/java/com/jeetatl/datamasking/maskers/QueryParameterMasker.java
+++ src/main/java/com/jeetatl/datamasking/maskers/QueryParameterMasker.java
@@ -1,7 +1,75 @@
 package com.jeetatl.datamasking.maskers;
 
+import com.jeetatl.datamasking.config.MaskingConfiguration;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.Map;
+
 /**
- * Created by mkhokhar on 11/28/16.
+ * <p>{@code QueryParameterMasker} is a concrete class used to apply {@link MaskingConfiguration} to
+ * query parameters.</p>
+ *
+ * The masking is applied to any query parameter value that is currently configured within the
+ * {@link MaskingConfiguration}.
  */
 public class QueryParameterMasker {
+
+    MaskingConfiguration config = null;
+
+    /**
+     *  Constructs a {@code QueryParameterMasker} with the provided configuration settings.
+     * @param config Configuration settings to use with this {@code QueryParameterMasker}.
+     */
+    public QueryParameterMasker(MaskingConfiguration config) {
+        this.config = config;
+    }
+
+    /**
+     * This method is used to apply the masking settings to the query parameters.
+     * @param payload A string containing valid query parameters.
+     * @return A string containing query parameter payload after the masking settings have been applied.
+     */
+    public String getMasked(String payload) {
+        if (payload == null || payload.isEmpty()) {
+            return payload;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            String[] tokens = payload.split("&");
+            for (int i = 0; i < tokens.length; i++) {
+                String[] paramAndValue = tokens[i].split("=");
+                if (paramAndValue.length == 2) {
+                    sb.append(paramAndValue[0] + "=" + URLEncoder.encode(config.apply(URLDecoder.decode(paramAndValue[0], "UTF-8"), URLDecoder.decode(paramAndValue[1], "UTF-8")), "UTF-8"));
+                }
+                if (i != tokens.length - 1) {
+                    sb.append("&");
+                }
+            }
+        } catch (Exception e) {
+            return payload;
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * @return The masking configuration
+     */
+    public MaskingConfiguration getConfig() {
+        return config;
+    }
+
+    /**
+     * Set the masking configuration.
+     * @param config Masking configuration to set.
+     */
+    public void setConfig(MaskingConfiguration config) {
+            this.config = config;
+        }
+
 }
--- src/test/java/com/jeetatl/datamasking/maskers/QueryParameterMaskerTests.java
+++ src/test/java/com/jeetatl/datamasking/maskers/QueryParameterMaskerTests.java
@@ -1,7 +1,103 @@
 package com.jeetatl.datamasking.maskers;
 
-/**
- * Created by mkhokhar on 11/28/16.
- */
+import com.jeetatl.datamasking.config.MaskingConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class QueryParameterMaskerTests {
+
+    @Test
+    public void testConstructor() {
+        MaskingConfiguration config = new MaskingConfiguration("apple:~3");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+        Assert.assertEquals(config, masker.getConfig());
+    }
+
+    @Test
+    public void testConfigSetter() {
+        MaskingConfiguration config = new MaskingConfiguration("apple:~3");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        MaskingConfiguration config2 = new MaskingConfiguration("orange:~5");
+        masker.setConfig(config2);
+
+        Assert.assertEquals(config2, masker.getConfig());
+    }
+
+    @Test
+    public void testSimpleQueryMasking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+100%");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = "field1=queryparam1value";
+        String maskedField = "field1=XXXXXXXXXXXXXXXX";
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
+
+    @Test
+    public void testMultipleSimpleQueryMasking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+100%|field2:-2");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = "field1=queryparam1value&field2=field2value";
+        String maskedField = "field1=XXXXXXXXXXXXXXXX&field2=field2valXX";
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
+
+    @Test
+    public void testComplexQueryMasking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+100%|field2:-2");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = "field1=queryparam%201value&field2=field2value";
+        String maskedField = "field1=XXXXXXXXXXXXXXXXX&field2=field2valXX";
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
+
+    @Test
+    public void testComplexQuery2Masking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+3|field2:-2");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = "field1=queryparam+1value&field2=field2value%25%26";
+        String maskedField = "field1=XXXryparam+1value&field2=field2valueXX";
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
+
+    @Test
+    public void testBadQueryMasking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+3|field2:-2");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = "field1=%z9";
+        String maskedField = stringToMask;
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
+
+    @Test
+    public void testNullQueryMasking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+3|field2:-2");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = null;
+        String maskedField = null;
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
+
+    @Test
+    public void testEmptyQueryMasking() {
+        MaskingConfiguration config = new MaskingConfiguration("field1:+3|field2:-2");
+        QueryParameterMasker masker = new QueryParameterMasker(config);
+
+        String stringToMask = "";
+        String maskedField = "";
+
+        Assert.assertEquals(maskedField, masker.getMasked(stringToMask));
+    }
 }
